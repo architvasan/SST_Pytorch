2ffee4ff1332d71f52762282ecc45056bc482eb2
Archit Vasan <avasan@lambda0.cels.anl.gov>
2024-06-11 12:44:37 -0700

    added scripts

--- sst_reg.py
+++ sst_reg.py
@@ -13,7 +13,6 @@ import torch
 from torch.utils.data import Dataset, DataLoader, dataset
 import torch.utils.data as data_utils
 import matplotlib.pyplot as plt
-import seaborn as sns
 import math
 import os
 from tempfile import TemporaryDirectory
@@ -29,50 +28,6 @@ import warnings
 from torcheval.metrics.functional import multiclass_f1_score
 from torcheval.metrics import BinaryAccuracy
 warnings.filterwarnings("ignore")
-'''
-Initialize tokenizer
-'''
-vocab_file = 'VocabFiles/vocab_spe.txt'
-spe_file = 'VocabFiles/SPE_ChEMBL.txt'
-tokenizer = SMILES_SPE_Tokenizer(vocab_file=vocab_file, spe_file= spe_file)
-
-def pad_infinite(iterable, padding=None):
-   return chain(iterable, repeat(padding))
-
-def pad(iterable, size, padding=None):
-   return islice(pad_infinite(iterable, padding), size)
-
-def tokenize_function(examples):
-    #print(examples[0])
-    return np.array(list(pad(tokenizer(examples)['input_ids'], 25, 0)))
-
-def training_data(raw_data):
-    smiles_data_frame = pd.DataFrame(data = {'text': raw_data['Canonical SMILES'], 'labels': raw_data['Toxicity Value']})
-    print(smiles_data_frame['text'])
-    smiles_data_frame['text'] = smiles_data_frame['text'].map(tokenize_function)#, batched=True)
-    print(smiles_data_frame['text'].values)
-    target = smiles_data_frame['labels'].values
-    features = np.stack([tok_dat for tok_dat in smiles_data_frame['text']])
-    print(target)
-    #train = data_utils.TensorDataset(features, target)
-    #train_loader = data_utils.DataLoader(train, batch_size=10, shuffle=True)
-    feature_tensor = torch.tensor(features)
-    label_tensor = torch.tensor(smiles_data_frame['labels'])
-    print(feature_tensor)
-    print(label_tensor)
-    dataset = TensorDataset(feature_tensor, label_tensor)
-    print(len(dataset[0][0]))
-    train_size = int(0.9 * len(dataset))
-    test_size = int(len(dataset) - train_size)
-
-    training_data, test_data = torch.utils.data.random_split(dataset, [train_size, test_size])
-    train_dataloader = DataLoader(training_data, batch_size=128, shuffle=True)
-    #print(training_data.shape)
-    print(len(test_data))
-    test_dataloader = DataLoader(test_data, batch_size=1024, shuffle=False)
-    return train_dataloader, test_dataloader, test_data
-
-
 class PositionalEncoding(nn.Module):
 
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
@@ -97,11 +52,13 @@ class PositionalEncoding(nn.Module):
 class TransformerModel(nn.Module):
 
     def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
-                 nlayers: int, dropout: float = 0.1):
+                 nlayers: int, dropout: float, device):
         super().__init__()
+        self.device = device
         self.model_type = 'Transformer'
         self.pos_encoder = PositionalEncoding(d_model, dropout)
-
+        self.ntoken = ntoken
+        self.d_model = d_model
         encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
         
         self.transformer_encoder1 = TransformerEncoder(encoder_layers, nlayers)
@@ -111,7 +68,7 @@ class TransformerModel(nn.Module):
         self.d_model = d_model
 
         self.dropout1 = nn.Dropout(0.2)
-        self.linear1 = nn.Linear(6400, 2048)
+        self.linear1 = nn.Linear(self.d_model*self.ntoken, 2048)
         self.act1 = nn.ReLU()
 
         self.dropout2 = nn.Dropout(0.2)
@@ -165,12 +122,12 @@ class TransformerModel(nn.Module):
             """Generate a square causal mask for the sequence. The masked positions are filled with float('-inf').
             Unmasked positions are filled with float(0.0).
             """
-            src_mask = nn.Transformer.generate_square_subsequent_mask(len(src)).to(device)
+            src_mask = nn.Transformer.generate_square_subsequent_mask(len(src)).to(self.device)
         output = self.transformer_encoder1(src, src_mask)
         output = self.transformer_encoder2(output)
         #output = self.layer_norm(output)
         output = self.dropout1(output)
-        output = torch.reshape(output, (len(output),len(output[0])*len(output[0][0])))
+        output = torch.reshape(output, (len(output), self.d_model*self.ntoken))# (len(output),len(output[0])*len(output[0][0])))
         output = self.linear1(output)
         output = self.act1(output)
         output = self.dropout2(output)
@@ -182,79 +139,14 @@ class TransformerModel(nn.Module):
         output = self.dropout4(output)
         output = self.linear4(output)
         #output = torch.sigmoid(output)
-        output = torch.act4(output)
+        output = self.act4(output)
         output = self.dropout5(output)
         output = self.linear5(output)
-        output = torch.act5(output)
+        output = self.act5(output)
         output = self.dropout6(output)
         output = self.linear6(output)
-        output = torch.act6(output)
+        #output = self.act6(output)
         #output = self.act5(output)
-        return torch.reshape(output, (-1,))
+        return output#torch.reshape(output, (-1,))
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-directory = 'data'
-dir_list = os.listdir(directory)
-
-'''
-Single task learning tests
-'''
-if True:
-    for fil in dir_list:
-        raw = pd.read_csv(f'{directory}/{fil}')
-        #raw = pd.read_csv(f'data/Endocrine_Disruption_NR-AR.csv')
-        base = Path(f'{fil}').stem
-    
-        train_loader, test_loader, test_data = training_data(raw)
-        
-        model = TransformerModel(ntoken=64, d_model=256, nhead=16, d_hid=256,
-                         nlayers=8, dropout= 0.2)
-        model = model.to(device) 
-        
-        # define optimizer. Specify the parameters of the model to be trainable. Learning rate of .001
-        optimizer = torch.optim.Adam(model.parameters(), lr = 1e-5)
-        loss_fn = nn.MSELoss()#nn.BCELoss()#NLLLoss() #nn.
-        
-        # some extra variables to track performance during training
-        f1_history = []
-        f1_vals = []
-        trainstep = 0
-        running_loss = 0.
-        metric = BinaryAccuracy()
-        for i in tqdm(range(50)):
-            for j, (batch_X, batch_y) in enumerate(train_loader):
-                preds = model(batch_X.to(device))
-                #print(preds)
-                loss = loss_fn(preds, batch_y.float().to(device))
-                #print(loss)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-        
-            for k, (batch_Xt, batch_yt) in enumerate(test_loader):
-                y_hat = model(batch_Xt.to(device))
-                #print(y_hat)
-                y_hat =y_hat#(torch.round(y_hat)).long()#int64()# >=.5
-                #print(y_hat)
-                y_grnd = batch_yt.long().to(device)#==1
-                #print(y_grnd)
-                metric.update(y_hat, y_grnd)
-                acc_k = metric.compute()
-                #f1_k = multiclass_f1_score(y_hat, y_grnd, num_classes=2, average="macro")
-                f1_history.append({'epoch' : i, 'minibatch' : k, 'trainstep' : trainstep,
-                                          'task' : 'tox', 'binacc' : acc_k})
-                f1_vals.append(acc_k)
-                trainstep += 1
-            
-            if acc_k == np.max(f1_vals):
-                print(f1_history)
-                torch.save({
-                    'epoch': i,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'loss': loss,
-                    }, f'models/{base}.pt')
-    
-        f1_df = pd.DataFrame(f1_history)
-        f1_df.to_csv(f'f1_{base}.csv')
 
